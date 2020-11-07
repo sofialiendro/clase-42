efdb43b1c19b0bc46bf9e10954137ebca57e8fc1
Sofia Liendro <sofialiendro.s@gmail.com>
2020-11-07 15:54:56 -0300

    ejs

--- index.js
+++ index.js
@@ -314,3 +314,65 @@ while (continuarPrograma) {
   }
 
 }
+
+
+// Crear una función repetir que tome como argumento un valor valor y un número entero cantidad, y devuelva una array con valor repetido cantidad de veces.
+
+const repetir = (str, num) => {
+  const array = []
+  for (let i = 0; i < num; i++) {
+    array.push(str) 
+  }
+  return array
+}
+
+console.log(repetir('lovelace', 3)) // ['lovelace', 'lovelace', 'lovelace']
+console.log(array)
+console.log(repetir('a', 5)) // ['a', 'a', 'a', 'a', 'a']
+console.log(repetir('html', 0)) // []
+
+
+// Crear una función sumarImparesHasta que tome como argumento un número numero y que devuelva la suma de todos los impares empezando desde 0 hasta dicho numero inclusive.
+
+const sumarImparesHasta = (num) => {
+  // encontrar todos los numeros impares
+  // sumarlos a todos entre si
+  let acumuladora = 0 
+  for (let i = 1; i <= num; i += 2) {
+      acumuladora += i
+  }
+  return acumuladora
+
+}
+
+console.log(sumarImparesHasta(5)) // 9 (1 + 3 + 5 = 9)
+console.log(sumarImparesHasta(13)) // 49
+console.log(sumarImparesHasta(47)) // 576
+
+
+// ejemplos:
+
+let numero = 0
+// para sumar 1 en 1
+numero++
+//para restar 1 en 1
+numero--
+
+// para sumar o restar con otro numero (ej 2)
+
+numero+=2
+numero-=2
+
+
+// Crear una función crearCuentaRegresiva que tome como argumento un número entero numeroInicial y que devuelva un array con cuyo primer ítem sea numeroInicial y los demás ítems sean números enteros sucesivos descendientes, hasta llegar a 0.
+
+const crearCuentaRegresiva = (num) => {
+  let array = []
+  for (let i = num; i >= 0; i--) {
+    array.push(i)
+  }
+  return array
+}
+
+console.log(crearCuentaRegresiva(3)) // [3, 2, 1, 0]
+console.log(crearCuentaRegresiva(5)) // [5, 4, 3, 2, 1, 0]
